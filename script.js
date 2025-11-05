6cab5295eec844e334ca35504b5e641be1a641bd
mohabroha@mail.de <mohabroha@mail.de>
2025-11-05 10:23:48 +0100

    async function Committed

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const pokemonContainer = document.getElementById('pokemon-container');
 let results = [];
 
 function init() {
-
+    loadPokemon();
 }
-
 async function loadPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=6');
+    results = (await response.json()).results;
 
+    let html = '';
+    for (let i = 0; i < results.length; i++) {
+        const pokemon = results[i];
+        html += `
+            <div class="pokemon-card" onclick="showModal('${pokemon.name}')">
+                <p>${pokemon.name}</p>
+            </div>
+        `;
+    }
+    pokemonContainer.innerHTML = html;
 }
 
-function renderPokemon() {
-
-}
 
 function showModal(name) {
 
@@ -21,3 +29,5 @@ function showModal(name) {
 function closeModal() {
 
 }
+
+init();
