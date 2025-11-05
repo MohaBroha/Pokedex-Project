87c0b4a18681e54a0e61b1d6c7788e680243a976
mohabroha@mail.de <mohabroha@mail.de>
2025-11-05 09:51:13 +0100

    script files created

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const modalContainer = document.getElementById('modal-container');
+const pokemonContainer = document.getElementById('pokemon-container');
+let results = [];
 
+function init() {
+
+}
+
+async function loadPokemon() {
+
+}
+
+function renderPokemon() {
+
+}
+
+function showModal(name) {
+
+}
+
+function closeModal() {
+
+}
