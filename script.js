5d2a0a84cd6f2ba3f06f618fedc759cb96f77ff9
mohabroha@mail.de <mohabroha@mail.de>
2025-11-05 12:35:05 +0100

    renderFunction added

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ let results = [];
 function init() {
     loadPokemon();
 }
-async function loadPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=6');
-    results = (await response.json()).results;
 
+async function loadPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+    results = (await response.json()).results;
+    renderPokemon(results);
+}
+
+function renderPokemon(pokemonList) {
     let html = '';
-    for (let i = 0; i < results.length; i++) {
-        const pokemon = results[i];
+    for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemonList[i];
+        const id = pokemon.url.split('/').filter(Boolean).pop();
+        const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
         html += `
-            <div class="pokemon-card" onclick="showModal('${pokemon.name}')">
+            <div class="pokemon-card" onclick="showModal('${pokemon.name}', '${img}')">
+                <img src="${img}" alt="${pokemon.name}" />
                 <p>${pokemon.name}</p>
             </div>
         `;
@@ -22,7 +30,12 @@ async function loadPokemon() {
 }
 
 
-function showModal(name) {
+function filterPokemon() {
+
+}
+
+
+function showModal(name, img) {
 
 }
 
