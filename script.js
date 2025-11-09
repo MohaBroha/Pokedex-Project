d406ca20a635b826b866b02144e4d419305c9613
mohabroha@mail.de <mohabroha@mail.de>
2025-11-10 00:57:48 +0100

    newFunction added / supplemented

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function getPokemonDetails(pokemon) {
         name: pokemon.name,
         id: data.id,
         types: data.types.map(t => t.type.name),
-        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
+        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`
     };
 }
 
@@ -28,16 +28,14 @@ async function renderPokemon(pokemonList) {
     for (let i = 0; i < pokemonList.length; i++) {
         const details = await getPokemonDetails(pokemonList[i]);
         html += `
-          <div class="pokemon-card" onclick="showModal('${details.name}', '${details.img}')">
-    <img src="${details.img}" alt="${details.name}" />
-    <div class="pokemon-info">
-        <div class="pokemon-top">
-            <span class="pokemon-types">${details.types.join(', ')}</span>
-            <span class="pokemon-id">ID: ${details.id}</span>
-        </div>
-        <p class="pokemon-name">${details.name.toUpperCase()}</p>
-    </div>
-</div>
+            <div class="pokemon-card" onclick="showModalWithStats('${details.name}', '${details.img}', '${pokemonList[i].url}')">
+                <img src="${details.img}" alt="${details.name}" />
+                <div class="pokemon-info">
+                    <p class="pokemon-name">${details.name.toUpperCase()}</p>
+                    <p class="pokemon-types">${details.types.join(', ')}</p>
+                    <p class="pokemon-id">ID: ${details.id}</p>
+                </div>
+            </div>
         `;
     }
     pokemonContainer.innerHTML = html;
@@ -57,12 +55,96 @@ function filterPokemon() {
 
 
 function showModal(name, img) {
+    modalContainer.innerHTML = `
+        <div class="modal-backdrop" onclick="closeModal()">
+            <div class="modal-content">
+                <h2>${name.toUpperCase()}</h2>
+                <img src="${img}" alt="${name}" />
+                 <p class="pokemon-types">${details.types.join(', ')}</p>
+                    <p class="pokemon-id">ID: ${details.id}</p>
+                <button onclick="closeModal()">Schließen</button>
+            </div>
+        </div>
+    `;
 
 }
+
+async function showModalWithStats(name, img, url) {
+    // Details inkl. Stats von der API laden
+    const response = await fetch(url);
+    const data = await response.json();
+    const stats = data.stats.map(stat => ({
+        name: stat.stat.name,
+        value: stat.base_stat
+    }));
+
+    // Modal-Inhalt mit Chart
+    modalContainer.innerHTML = `
+        <div class="modal-backdrop" onclick="closeModal()">
+            <div class="modal-content" onclick="event.stopPropagation()">
+                <h2>${name.toUpperCase()}</h2>
+                <img src="${img}" alt="${name}" />
+                
+
+                <canvas id="pokemonChart" width="300" height="200"></canvas>
+
+                <button onclick="closeModal()">Schließen</button>
+            </div>
+        </div>
+    `;
+    document.body.classList.add('modal-open');
+
+    // Chart erstellen
+    const ctx = document.getElementById('pokemonChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: stats.map(s => s.name.toUpperCase()),
+            datasets: [{
+                label: 'Base Stats',
+                data: stats.map(s => s.value),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(255, 159, 64, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: { stepSize: 10 }
+                }
+            },
+            plugins: {
+                legend: { display: false }
+            }
+        }
+    });
+
+}
+
+
+
 
 function closeModal() {
     modalContainer.innerHTML = '';
     modalContainer.className = '';
+
 }
 
 
